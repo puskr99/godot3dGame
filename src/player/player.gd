1356869371fcff257811335bf4c2e15e7c129c57
puskr99 <puskaradhikari65@gmail.com>
2024-05-25 21:32:07 +0545

    room made

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,39 +1,32 @@
 extends CharacterBody3D
 
-var speed
-const WALK_SPEED = 2.0
-const RUN_SPEED = 4.0
-#const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.004
-var is_running: bool = false
+@export var walk_speed = 2.0
+@export var run_speed = 4.0
+@export var sensi_horiz = 0.2
+@export var sensi_verti = 0.2
 
-const BOB_FREQ = 2.4
-const BOB_AMP = 0.08
-var t_bob = 0.0
-
-const BASE_FOV = 75.0
-const FOV_CHANGE = 1.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 9.8
+var speed
+var is_running: bool = false
+var is_kicking: bool = false
+var is_fpp: bool = false
 
-@onready var animation_player = $base_player/AnimationPlayer
-@onready var head = $base_player
-@onready var camera = $SpringArm3D/Camera3D
+@onready var animation_player = $Visauls/base_player/AnimationPlayer
+@onready var visauls = $Visauls
+@onready var camera_mount = $CameraMount
 
 
-func _ready():
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-func _unhandled_input(event):
+func _unhandled_input(event) -> void:
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		rotate_y(deg_to_rad(-event.relative.x * sensi_horiz))
+		visauls.rotate_y(deg_to_rad(event.relative.x * sensi_horiz))
+		camera_mount.rotate_y(deg_to_rad(-event.relative.y * sensi_verti))
 
 
-var is_kicking = false
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -52,18 +45,20 @@ func _physics_process(delta):
 		
 	# Handle Sprint.
 	if Input.is_key_pressed(KEY_SPACE):
-		speed = RUN_SPEED
+		speed = run_speed
 		is_running = true
 	else:
-		speed = WALK_SPEED
+		speed = walk_speed
 		is_running = false
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if is_on_floor():
 		if direction:
+			visauls.look_at(position + direction)
+			
 			if is_running:
 				if animation_player.current_animation != "running":
 					animation_player.play("running")
@@ -83,21 +78,15 @@ func _physics_process(delta):
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 	
-	# Head bob
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
-	
-	# FOV
-	var velocity_clamped = clamp(velocity.length(), 0.5, RUN_SPEED * 2)
-	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		is_fpp = !is_fpp
+		_change_prespective()
 
 
-func _headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQ) * BOB_AMP
-	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
-	return pos
-
+func _change_prespective() -> void:
+	if is_fpp:
+		$CameraMount/Camera3D.position = Vector3(0, 0.64, 2.098)
+	else:
+		$CameraMount/Camera3D.position = Vector3(0, 0.64, 0.0)
